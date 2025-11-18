ecd128d52ebc237a2997ef042d3345f05192ae20
Vaishali <rawatvaishali254@gmail.com>
2025-11-18 15:02:21 +0530

    updated the api url

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,7 +14,7 @@ const Body = () => {
     }, []);
 
     const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.59486030415836&lng=77.24561601877213&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.677067306891495&lng=77.29431621730328&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         console.log(json);
         setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
