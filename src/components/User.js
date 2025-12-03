2d3ca5bd20d40281c2813085add1fee2925eb71a
Vaishali <rawatvaishali254@gmail.com>
2025-12-03 23:22:28 +0530

    learning - how to create state variables and update in a class-based components

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const User = ({name}) => {
+    const [first, setFirst] = useState(0);
+    const [second] = useState(1);
     return (
         <div className="user-card">
             <h1><u>It is a functional component</u></h1>
+            <h1>Count = {first}</h1>
+            <button onClick={() => setFirst(first + 1)}>Click Me</button>
+            {/* <h1>Count = {second}</h1> */}
             <h2>Name : {name}</h2>
             <h3>Location : Delhi</h3>
             <h4>Email : rawatvaishali254@gmail.com</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,12 +5,21 @@ class UserClass extends React.Component{
         super(props);
 
         // console.log(props);
+
+        this.state = {
+            first : 0,
+            second : 1
+        }
     }
     render(){
         const {name} = this.props; //destructuring
+        const {first, second} = this.state;
         return(
             <div className="user-card">
                 <h1><u>It is a class-based component</u></h1>
+                <h1>Count : {first}</h1>
+                <button onClick={() => {this.setState({first : this.state.first + 1})}}>Click Me</button>
+                {/* <h1>Count : {second}</h1> */}
                 <h2>Name : {name}</h2>
                 <h3>Location : Delhi</h3>
                 <h4>Email : rawatvaishali254@gmail.com</h4>
