09b222435a8088730ad83dfd208130430137d07a
Vaishali <rawatvaishali254@gmail.com>
2025-12-09 20:41:31 +0530

    created custom hook to separate API work

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,22 +1,8 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-// import { MENU_API } from "../utils/constants";
-// import { useParams } from "react-router";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-    const [resInfo, setResInfo] = useState(null);
-    // const { resId } = useParams();
-
-    useEffect(() => {
-        fetchMenu();
-    }, []);
-
-    const fetchMenu = async () => {
-        const data = await fetch( "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7041&lng=77.1025&restaurantId=");
-        const json = await data.json();
-        console.log(json);
-        setResInfo(json.data);
-    };
+    const resInfo = useRestaurantMenu(resId);
 
     if(resInfo === null){
         return <Shimmer/>
---
+++ src/utils/useRestaurantMenu.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useRestaurantMenu = (resId) => {
+    const [resInfo, setResInfo] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7041&lng=77.1025&restaurantId=");
+        const json = await data.json();
+        setResInfo(json.data);
+    }
+    return resInfo;
+}
+
+export default useRestaurantMenu;
